b5a17e464dcaa8b7d0645d0ab448741c922fefb6
Kirtan Patel <kirtan_2605@yahoo.com>
2024-05-15 22:13:13 +0200

    Update inter-satellite_distance_analysis.m

--- src/Starlink Analysis MATLAB Files/inter-satellite_distance_analysis.m
+++ src/Starlink Analysis MATLAB Files/inter-satellite_distance_analysis.m
@@ -2,7 +2,7 @@ clc
 clear
 close all
 
-load("interSat_distances.mat")
+load("interSat_distances_data_v1.mat")
 load("LOS_visibility_data_v1.mat")
 
 
@@ -17,9 +17,7 @@ endDateTime = datetime(2023, 1, 31, 0, 0, 0); % 30th Jan 2023, 23:59:00
 % Generate datetime vector with 1 minute timestep
 dateTimeVector = startDateTime:minutes(1):endDateTime;
 
-
-
-LOS_visible_distances = LOS_visibility_data.*interSatdistancesdata;
+LOS_visible_distances = LOS_visibility_data_v1.*interSat_distances_data_v1;
 
 % Replace zeros with inf, so that they dont get counted as minimum distances
 LOS_visible_distances(LOS_visible_distances == 0) = inf;
@@ -37,6 +35,8 @@ grid on;
 grid minor
 ax=gca;
 ax.FontSize = 15;
+
+
 
 %% Visible Satellite Distance Heatmap
 
@@ -61,11 +61,28 @@ colormap(cmap);
 colorbar;
 
 % Specify y-axis ticks
-yticks(linspace(1, size(LOS_visibility_data, 1), 5)); % Evenly spaced ticks
+yticks(linspace(1, size(LOS_visibility_data_v1, 1), 5)); % Evenly spaced ticks
 yticklabels({'1 Jan', '8 Jan', '15 Jan', '22 Jan', '30 Jan'}); % Specify the labels for the ticks
 ax=gca;
-ax.FontSize = 15;
+ax.FontSize = 18;
+
 
 % Set axis labels
 xlabel('Starlink Satellie Number');
 title('Inter-satellite Distance [km] 2023');
+
+
+% create a new pair of axes inside current figure
+axes('position',[.60 .65 .25 .25])
+box on % put box around new pair of axes
+%indexOfInterest = (XDates < '06-Dec-2023 06:19:00') & (XDates > '05-Dec-2023 23:19:00');
+% Create heatmap
+imagesc(LOS_visible_distances(1:200, 1:17)/1000);    % in km
+yticks('');
+xticks('auto');
+xtickangle(45);
+grid on
+grid minor
+axis tight
+axes2=gca;
+axes2.FontSize = 18;
